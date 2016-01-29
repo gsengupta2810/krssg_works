42bc3e4ffba58cc69005231321fae75d4ed933f6
gsengupta2810 <gsengupta2810@gmail.com>
2016-01-29 19:17:42 +0530

    first-changed commit

--- velocity_conv.cpp
+++ velocity_conv.cpp
@@ -3,12 +3,12 @@ using namespace std;
 
 int main()
 {
-	float vel_eucX[3],vel_eucY[2];
+	float vel_euc[3];
 	float vel_wheel[4];
 	float R;
 
-	vel_wheel[0]=-0.5*vel_eucX[0]+0.866*vel_eucY[0]+vel_eucX[3]*R;
-	vel_wheel[1]=-0.5*vel_eucX[0]-0.866*vel_eucY[0]+vel_eucX[3]*R;
-	vel_wheel[2]=0.707*vel_eucX[0]-0.707*vel_eucY[0]+vel_eucX[3]*R;
-	vel_wheel[3]=0.707*vel_eucX[0]+0.707*vel_eucY[0]+vel_eucX[3]*R;
+	vel_wheel[0]=-0.5*vel_euc[0]+0.866*vel_euc[1]+vel_euc[2]*R;
+	vel_wheel[1]=-0.5*vel_euc[0]-0.866*vel_euc[1]+vel_euc[2]*R;
+	vel_wheel[2]=0.707*vel_euc[0]-0.707*vel_euc[1]+vel_euc[2]*R;
+	vel_wheel[3]=0.707*vel_euc[0]+0.707*vel_euc[1]+vel_euc[2]*R;
 }
